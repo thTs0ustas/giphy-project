513ee1aca9d1a19b24156e797399469b9e44c163
thTs0ustas <th.ts0ustas@gmail.com>
2020-04-19 23:03:12 +0300

    functional-success-dispatch

--- src/components/searchResults/SearchResults.js
+++ src/components/searchResults/SearchResults.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SearchResult } from "../searchResult/SearchResult";
 
 export const SearchResults = ({ results }) => {
+  console.log(results);
   return (
     <div>
       {results.map((result) => (
